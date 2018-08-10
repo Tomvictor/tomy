f09d5a796342762360d6085119ec5d29d942851f
Tom Victor <vjtomvictor@icloud.com>
2018-08-10 20:31:23 +0530

    utils added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tomy",
-    version="0.0.2",
+    version="0.0.4",
     author="Tom Victor",
     author_email="vjtomvictor@gmail.com",
     description="Bunch of useful python codes",
